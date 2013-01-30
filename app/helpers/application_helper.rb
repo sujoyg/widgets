fd4f511bc092e2c02111c04e431ab3ecda840e46
Sujoy Gupta <sujoyg@gmail.com>
2013-01-29 17:30:30 -0800

    Rename component to widget everywhere.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
-  def component(name, options={}, &block)
+  def widget(name, options={}, &block)
     id = Guid.new.to_s
-    partial = File.join 'components', name.to_s
-    attributes = {:id => id, :'data-component' => name, :style => options.delete(:style), :class => options.delete(:effects)}
+    partial = File.join 'widgets', name.to_s
+    attributes = {:id => id, :'data-widget' => name, :style => options.delete(:style), :class => options.delete(:effects)}
     contents = if block_given?
                  render :layout => partial, :inline => capture(&block), :locals => options
                else
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,15 +2,15 @@ require 'spec_helper'
 require 'guid'
 
 describe ApplicationHelper do
-  describe '#component' do
+  describe '#widget' do
     let(:contents) { random_text :length => 256 }
     let(:name) { File.join random_text, random_text }
     let(:options) { {random_text => random_time, random_text => random_url} }
 
-    it 'should render the specified component as a template for its content.' do
-      view.should_receive(:render).with(hash_including :template => "components/#{name}").and_return(contents)
+    it 'should render the specified widget as a template for its content.' do
+      view.should_receive(:render).with(hash_including :template => "widgets/#{name}").and_return(contents)
 
-      helper.component(name, options).should contain contents
+      helper.widget(name, options).should contain contents
     end
 
     it 'should render a div with a unique id.' do
@@ -18,12 +18,12 @@ describe ApplicationHelper do
       Guid.stub!(:new).and_return(id = Guid.new.to_s)
       view.stub! :render
 
-      helper.component(name, options).should have_selector 'div', :id => id
+      helper.widget(name, options).should have_selector 'div', :id => id
     end
 
-    it 'should include the component name as a data attribute.' do
+    it 'should include the widget name as a data attribute.' do
       view.stub! :render
-      helper.component(name, options).should have_selector 'div', :'data-component' => name
+      helper.widget(name, options).should have_selector 'div', :'data-widget' => name
     end
 
     describe 'style' do
@@ -33,12 +33,12 @@ describe ApplicationHelper do
         it 'renders a div with the specified style.' do
           view.stub! :render
 
-          helper.component(name, options.merge(:style => style)).should have_selector 'div[style]', :style => style
+          helper.widget(name, options.merge(:style => style)).should have_selector 'div[style]', :style => style
         end
 
         it 'passes all options excluding the width as locals to the content template.' do
           view.should_receive(:render).with(hash_including :locals => options)
-          helper.component name, options.merge(:style => style)
+          helper.widget name, options.merge(:style => style)
         end
       end
 
@@ -46,12 +46,12 @@ describe ApplicationHelper do
         it 'renders a div without any style.' do
           view.stub! :render
 
-          helper.component(name, options).should_not have_selector 'div[style]'
+          helper.widget(name, options).should_not have_selector 'div[style]'
         end
 
         it 'passes all options as locals to the content template.' do
           view.should_receive(:render).with(hash_including :locals => options)
-          helper.component(name, options)
+          helper.widget(name, options)
         end
       end
     end
@@ -63,12 +63,12 @@ describe ApplicationHelper do
         it 'renders a div with the specified effects as its class.' do
           view.stub! :render
 
-          helper.component(name, options.merge(:effects => effects)).should have_selector 'div[class]', :class => effects
+          helper.widget(name, options.merge(:effects => effects)).should have_selector 'div[class]', :class => effects
         end
 
         it 'passes all options excluding the width as locals to the content template.' do
           view.should_receive(:render).with(hash_including :locals => options)
-          helper.component name, options.merge(:effects => effects)
+          helper.widget name, options.merge(:effects => effects)
         end
       end
 
@@ -76,12 +76,12 @@ describe ApplicationHelper do
         it 'renders a div without any class.' do
           view.stub! :render
 
-          helper.component(name, options).should_not have_selector 'div[class]'
+          helper.widget(name, options).should_not have_selector 'div[class]'
         end
 
         it 'passes all options as locals to the content template.' do
           view.should_receive(:render).with(hash_including :locals => options)
-          helper.component(name, options)
+          helper.widget(name, options)
         end
       end
     end
