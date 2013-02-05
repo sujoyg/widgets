0bfb86efb329b74fef320256262d314da4b298d7
Sujoy Gupta <sujoyg@gmail.com>
2013-02-04 23:01:22 -0800

    Fix: Always lookup html layouts for widgets, even if rendering in say, js mode.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,8 +4,9 @@ module ApplicationHelper
     partial = File.join 'widgets', name.to_s
     attributes = {:id => id, :'data-widget' => name, :style => options.delete(:style), :class => options.delete(:effects)}
     options[:widget_id] = id unless options.include? :widget_id
+
     contents = if block_given?
-                 render :layout => partial, :inline => capture(&block), :locals => options
+                 render :layout => partial, :inline => capture(&block), :locals => options, :formats => [:html]
                else
                  render :template => partial, :locals => options
                end
